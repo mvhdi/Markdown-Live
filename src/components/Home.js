ee0de43daa022f4d245dacf1acd57cefa551d4da
Mahdi H Mohamed <mr.mahdimohamediii@gmail.com>
2019-07-04 21:29:02 -0500

    connected to live firebase database

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,26 @@ import uuid from "uuid/v4";
 class Home extends Component {
   state = {
     // collections of posts
-    posts: []
+    posts: [],
+    title: ""
   };
+  componentDidMount() {
+    // connect to firebase db in and pass the collection "/posts"
+    // .on listens to changes in this db
+    db.ref("/posts").on("value", snapshot => {
+      // initialize posts array for all posts in our db
+      let posts = [];
+      // loop though the callback snapshot, and populate our posts array with data from db
+      snapshot.forEach(childSnapshot => {
+        // also extract the unique id for each post
+        posts.push({ ...childSnapshot.val(), key: childSnapshot.key });
+      });
+      // set our posts variable
+      this.setState({ posts });
+    });
+  }
   render() {
+    console.log(this.state.posts);
     return (
       <div>
         <h1> Home </h1>
